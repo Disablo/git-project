ac66e5c49c044f9cb91a878dbac81ca31dd217d5
Unit <disablo56@gmail.com>
2018-07-20 23:18:17 +0300

    New bin dec version

--- projects/from bin to dec/from-binary_to-decimal.py
+++ projects/from bin to dec/from-binary_to-decimal.py
@@ -1,4 +1,5 @@
-"""Программа предназначего для перевода чисел из бинарных в десятичные и обратно"""
+"""Программа предназначего для перевода чисел из бинарных в десятичные и обратно
+    20/07/2018 добавлена проверка пользовательского ввода"""
 
 res = ""
 b = None
@@ -7,7 +8,14 @@ g = 0 # for calc index of arr
 e = 0 # transient var for bin->dec
 arr_d = []
 arr_b = []
+user_input = ""
 user_choice = 0
+
+BI_CHAR = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ23456789qwertyuiop[]asdfghjkl;'\"\\zxcvbnm,." \
+          "/QWERTYUIOASDFGHJKLZXCVBNM`~!@#$%^&*()-=_+|<>"
+
+DE_CHAR = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮqwertyuiop[]asdfghjkl;'\"\\zxcvbnm,./QWE" \
+          "RTYUIOASDFGHJKLZXCVBNM`~!@#$%^&*()-=_+|<>"
 
 print("Привет! Я перевожу десятичные в двоичные и наоборот.\n")
 
@@ -15,7 +23,16 @@ while user_choice not in (1, 2):
     user_choice = int(input("Вы хотите двоичное перевести в десятичнок (1) или десятичное в двоичное (2): "))
 
 if user_choice == 1:
-    user_input = input("Введите двоичное, чтобы я перевёл его в десятичное: ")
+
+    while user_input in BI_CHAR:
+        user_input = input("Введите двоичное, чтобы я перевёл его в десятичное: ")
+
+        for i in user_input:
+            if i in BI_CHAR:
+                print("Ошибка ввода. Введите числа от 0 до 1.")
+                user_input = ""
+                break
+
     for i in user_input:
         arr_b.append(int(i))
 
@@ -30,8 +47,17 @@ if user_choice == 1:
     print(e)
 
 elif user_choice == 2:
-    user_input = int(input("Введите десятичное, чтобы я перевёл его в двоичное: "))
 
+    while user_input in DE_CHAR:
+        user_input = input("Введите десятичное, чтобы я перевёл его в двоичное: ")
+
+        for i in user_input:
+            if i in DE_CHAR:
+                print("Ошибка ввода. Введите числа от 0 до 9.")
+                user_input = ""
+                break
+
+    user_input = int(user_input)
     while b != 0:
         a = user_input % 2
         b = user_input // 2
